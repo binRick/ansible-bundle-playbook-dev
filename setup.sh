a50b6b48fcaaa97b05cc32cf820965cb281abeed
root <admin@vpntech.net>
2019-11-18 23:57:18 -0500

    add debug mode

--- setup.sh
+++ setup.sh
@@ -13,12 +13,16 @@ fi
     _DIR_BIN="$INSTALL_BASE/$1/ansible-playbook"
     _CMD="rm -rf $INSTALL_BASE/.tmp >/dev/null 2>&1; command mkdir -p $INSTALL_BASE/.tmp && command curl -s $_URL | command tar jxf - -C $INSTALL_BASE/.tmp && mv $INSTALL_BASE/.tmp/ansible-playbook $_DIR"
 if [[ ! -d "$INSTALL_BASE/$1" ]]; then
-    echo _URL=$_URL
-    echo _DIR=$_DIR
-    echo _DIR_BIN=$_DIR_BIN
-    echo _CMD=$_CMD
+    if [[ "$_DEBUG" == "1" ]]; then
+        echo _URL=$_URL
+        echo _DIR=$_DIR
+        echo _DIR_BIN=$_DIR_BIN
+        echo _CMD=$_CMD
+    fi
     if [[ ! -f "$_DIR_BIN" ]]; then
-        echo Downloading $1
+        if [[ "$_DEBUG" == "1" ]]; then
+            echo Downloading $1
+        fi
         eval bash -c "$_CMD"
         exit_code=$?
         if [[ "$exit_code" != "0" ]]; then
