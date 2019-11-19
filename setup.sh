69166755a273e3bba2189e20d0b3fbcb6c8b0208
root <admin@vpntech.net>
2019-11-19 00:10:49 -0500

    functional installer with validator

--- setup.sh
+++ setup.sh
@@ -4,20 +4,23 @@ set -m
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 INSTALL_BASE=~/.ansible-onedirs
 if [[ "$INSTALL_VERSION" != "" ]]; then
-    1="$INSTALL_VERSION"
+    _INSTALL_VERSION="$INSTALL_VERSION"
+else
+    _INSTALL_VERSION="$1"
 fi
-if [[ ! -d $INSTALL_BASE ]]; then
-    command mkdir -p $INSTALL_BASE
-fi
-if [[ "$1" == "" ]]; then
+if [[ "$INSTALL_VERSION" == "" ]]; then
     echo First Argument must be ansible version
     exit 1
 fi
-    _URL="https://raw.githubusercontent.com/binRick/ansible-bundle-playbook-dev/master/compiled/$1-onedir.tar.bz2"
-    _DIR="$INSTALL_BASE/$1"
-    _DIR_BIN="$INSTALL_BASE/$1/ansible-playbook"
+
+if [[ ! -d $INSTALL_BASE ]]; then
+    command mkdir -p $INSTALL_BASE
+fi
+    _URL="https://raw.githubusercontent.com/binRick/ansible-bundle-playbook-dev/master/compiled/$_INSTALL_VERSION-onedir.tar.bz2"
+    _DIR="$INSTALL_BASE/$_INSTALL_VERSION"
+    _DIR_BIN="$INSTALL_BASE/$_INSTALL_VERSION/ansible-playbook"
     _CMD="rm -rf $INSTALL_BASE/.tmp >/dev/null 2>&1; command mkdir -p $INSTALL_BASE/.tmp && command curl -s $_URL | command tar jxf - -C $INSTALL_BASE/.tmp && mv $INSTALL_BASE/.tmp/ansible-playbook $_DIR"
-if [[ ! -d "$INSTALL_BASE/$1" ]]; then
+if [[ ! -d "$INSTALL_BASE/$_INSTALL_VERSION" ]]; then
     if [[ "$_DEBUG" == "1" ]]; then
         echo _URL=$_URL
         echo _DIR=$_DIR
@@ -26,7 +29,7 @@ if [[ ! -d "$INSTALL_BASE/$1" ]]; then
     fi
     if [[ ! -f "$_DIR_BIN" ]]; then
         if [[ "$_DEBUG" == "1" ]]; then
-            echo Downloading $1
+            echo Downloading $_INSTALL_VERSION
         fi
         set +e
         eval bash -c "$_CMD"
