bce2037259caa172da1e30952fe41d2f1b2f3838
root <admin@vpntech.net>
2019-11-30 19:28:34 -0500

    auto sync

--- compiler/compileAnsible.sh
+++ compiler/compileAnsible.sh
@@ -154,9 +154,8 @@ replaceModuleName(){
 		s1="$(echo $r|cut -d'|' -f1)"
 		s2="$(echo $r|cut -d'|' -f2)"
 		if [[ "$s1" == "$_M" ]]; then
-			s1="^${s1}\$"
 			>&2 echo  changing _M=$_M based on r=$r, s1=$s1, s2=$s2
-			_M=$(echo $_M|sed -i "s/$s1/$s2/g")
+			_M=$(echo $_M|sed "s/^$s1/$s2/g")
 			>&2 echo _M changed to $_M
 			#exit
 		fi
