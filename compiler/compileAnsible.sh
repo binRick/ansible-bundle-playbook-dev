b73cd71fc636f1442b19c9a4789b9d96f3a95a42
root <admin@vpntech.net>
2019-11-30 22:22:19 -0500

    test sync

--- compiler/compileAnsible.sh
+++ compiler/compileAnsible.sh
@@ -17,7 +17,7 @@ ADDITIONAL_COMPILED_MODULES="terminaltables psutil loguru json2yaml setproctitle
 # watchdog psutil paramiko mysql-connector-python colorclass loguru requests python-jose pem pyopenssl pyyaml halo pymysql"
 # setproctitle linode-cli"
 # linode-cli"
-ADDITIONAL_COMPILED_MODULES_REPLACEMENTS="pyyaml|yaml python-jose|jose python_jose|jose pyopenssl|OpenSSL mysql-connector-python|mysql mysql_connector_python|mysql linode-cli|linodecli linode_cli|linodecli speedtest-cli|speedtestcli"
+ADDITIONAL_COMPILED_MODULES_REPLACEMENTS="pyyaml|yaml python-jose|jose python_jose|jose pyopenssl|OpenSSL mysql-connector-python|mysql mysql_connector_python|mysql linode-cli|linodecli linode_cli|linodecli speedtest-cli|speedtest"
 
 
 MODULE_BIN_INCLUDES=""
