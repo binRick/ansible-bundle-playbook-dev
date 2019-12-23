1009f01ac1b7ebf8b1acdc014368db8cec8b0de2
root <admin@vpntech.net>
2019-12-23 12:09:33 -0500

    add new callback plugin

--- compiler/compileAnsible.sh
+++ compiler/compileAnsible.sh
@@ -28,8 +28,9 @@ MODULE_BIN_TOTAL_INCLUDES_FILE=~/.MODULE_BIN_TITAL_INCLUDES.txt
 
 EXCLUDED_ADDITIONAL_MODULES="watchdog.utils.win32stat"
 EXCLUDED_ANSIBLE_MODULES="$EXCLUDED_ADDITIONAL_MODULES ansible.modules.network ansible.modules.cloud ansible.modules.remote_management ansible.modules.storage ansible.modules.web_infrastructure ansible.modules.windows ansible.module_utils.network ansible.plugins.doc_fragments ansible.plugins.terminal ansible.modules.net_tools ansible.modules.monitoring.zabbix ansible.modules.messaging ansible.modules.identity ansible.modules.database.postgresql ansible.modules.database.proxysql ansible.modules.database.vertica ansible.modules.database.influxdb ansible.modules.clustering ansible.modules.source_control.bitbucket ansible.module_utils.aws ansible.plugins.cliconf"
-#ADDITIONAL_ANSIBLE_CALLLBACK_MODULES="https://raw.githubusercontent.com/codekipple/ansible-callback-concise/master/callback_plugins/codekipple_concise.py" # https://raw.githubusercontent.com/binRick/ansible-beautiful-output/master/callback_plugins/beautiful_output.py
-ADDITIONAL_ANSIBLE_CALLLBACK_MODULES="https://raw.githubusercontent.com/codekipple/ansible-callback-concise/master/callback_plugins/codekipple_concise.py https://raw.githubusercontent.com/binRick/ansible-beautiful-output/master/callback_plugins/beautiful_output.py"
+
+
+ADDITIONAL_ANSIBLE_CALLLBACK_MODULES="https://raw.githubusercontent.com/codekipple/ansible-callback-concise/master/callback_plugins/codekipple_concise.py https://raw.githubusercontent.com/binRick/ansible-beautiful-output/master/callback_plugins/beautiful_output.py https://raw.githubusercontent.com/binRick/ansible-json-audit-log/master/callback/json_audit.py"
 ADDITIONAL_ANSIBLE_LIBRARY_MODULES="https://raw.githubusercontent.com/binRick/ansible-mysql-query/master/library/mysql_query.py https://raw.githubusercontent.com/ageis/ansible-module-ping/master/modules/icmp_ping.py https://raw.githubusercontent.com/cleargray/git_commit/master/git_commit.py"
 
 findFileImports(){
@@ -627,6 +628,21 @@ fi
         ANSIBLE_STDOUT_CALLBACK=unixy \
             testAnsible
 
+        >&2 echo "Executing Test Playbook with json_audit stdout callback"
+        ANSIBLE_JSON_AUDIT_LOG_FILE=$(mktemp)
+
+        ANSIBLE_ENVIRONMENT_NAME="test environment 123" \
+        ANSIBLE_JSON_LOG_PATH=$ANSIBLE_JSON_AUDIT_LOG_FILE \
+        ANSIBLE_DISPLAY_ARGS_TO_STDOUT=False \
+        ANSIBLE_STDOUT_CALLBACK=unixy \
+        ANSIBLE_CALLBACK_WHITELIST=json_audit \
+            testAnsible
+
+        ls -al $ANSIBLE_JSON_AUDIT_LOG_FILE
+        wc -l $ANSIBLE_JSON_AUDIT_LOG_FILE | grep -v "^0 "
+        grep -c '^{' /tmp/tmp.8FzjKQkWPd|grep -v '^0$'
+        grep -c '^{' /tmp/tmp.8FzjKQkWPd|head -n 5
+
         >&2 echo "Executing Test Playbook with codekipple_concise stdout callback"
         ANSIBLE_DISPLAY_ARGS_TO_STDOUT=False \
         ANSIBLE_STDOUT_CALLBACK=codekipple_concise \
@@ -637,10 +653,7 @@ fi
         ANSIBLE_STDOUT_CALLBACK=beautiful_output \
             testAnsible
 
-#        >&2 echo "Executing Test Playbook with beautiful_output stdout callback"
-#        ANSIBLE_DISPLAY_ARGS_TO_STDOUT=False \
-#        ANSIBLE_STDOUT_CALLBACK=beautiful_output \
-#            testAnsible
+
 
         cd $DIST_PATH
 
