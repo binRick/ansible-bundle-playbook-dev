38a507025f2096921ab22ea28daf047cb81b13dc
root <admin@vpntech.net>
2019-12-24 14:09:48 -0500

    automated sync

--- compiler/compileAnsible.sh
+++ compiler/compileAnsible.sh
@@ -42,7 +42,7 @@ MANUAL_HIDDEN_IMPORTS="--hidden-import=\"configparser\" \
 		    --hidden-import=\"logging.handlers\" \
 "
 
-ADDITIONAL_COMPILED_MODULES="terminaltables psutil loguru json2yaml setproctitle speedtest-cli pyyaml netaddr configparser"
+ADDITIONAL_COMPILED_MODULES="terminaltables psutil loguru json2yaml setproctitle speedtest-cli pyyaml netaddr configparser urllib3"
 ADDITIONAL_COMPILED_MODULES_REPLACEMENTS="pyyaml|yaml python-jose|jose python_jose|jose pyopenssl|OpenSSL mysql-connector-python|mysql mysql_connector_python|mysql linode-cli|linodecli linode_cli|linodecli speedtest-cli|speedtest"
 
 
