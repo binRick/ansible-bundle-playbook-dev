c45094cbdb5b2062369e5d3c476f0a6086002230
root <admin@vpntech.net>
2020-02-17 22:30:29 -0500

    automated sync

--- compiler/test2/mangleSpec.sh
+++ compiler/test2/mangleSpec.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 SPEC_FILE=$1
-
 _DIR=$(mktemp -d)
 
-grep '^block_cipher' $SPEC_FILE > $_DIR/block_cipher.txt
+if [[ ! -f "$SPEC_FILE" ]]; then
+    echo First Argument must be a spec file
+    exit
+fi
 
 ANALYSIS_START_LINE="$(grep '^a = Analysis(' test1.spec -n|cut -d':' -f1)"
 PYZ_START_LINE="$(grep '^pyz = PYZ(' test1.spec -n|cut -d':' -f1)"
-
 ANALYSIS_END_LINE="$(echo ${PYZ_START_LINE}-1|bc)"
-
 EXE_START_LINE="$(grep '^exe = EXE(p' test1.spec -n|cut -d':' -f1)"
 PYZ_END_LINE="$(echo ${EXE_START_LINE}-1|bc)"
 COLLECT_START_LINE="$(grep '^coll = COLLECT(' test1.spec -n|cut -d':' -f1)"
@@ -41,15 +40,20 @@ echo -e "\n\n"
 cmd="sed -n \"${ANALYSIS_START_LINE},${ANALYSIS_END_LINE}p\" test1.spec"
 eval $cmd > $_DIR/ANALYSIS.txt
 
+grep '^block_cipher' $SPEC_FILE > $_DIR/block_cipher.txt
 sed -n "${PYZ_START_LINE},${PYZ_END_LINE}p" test1.spec > $_DIR/PYZ.txt
 sed -n "${EXE_START_LINE},${EXE_END_LINE}p" test1.spec > $_DIR/EXE.txt
 sed -n "${COLLECT_START_LINE},${COLLECT_END_LINE}p" test1.spec > $_DIR/COLLECT.txt
 
 
 
-
+echo OK
 echo -e "\n\n"
 
 ls -al $_DIR
+echo -e "\n\n"
 wc -l $_DIR/*.txt
+echo -e "\n\n"
+echo Work Directory:
 echo $_DIR
+echo -e "\n\n"
