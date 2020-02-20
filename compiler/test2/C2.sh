3e8f46ea2d0882d25e286c43f7c879879d3c33c5
Your Name <you@example.com>
2020-02-19 20:58:08 -0500

    remove error checks

--- compiler/test2/C2.sh
+++ compiler/test2/C2.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #set -e
-(sudo useradd C2;) >/dev/null 2>&1
+[[ "$BUILD_USER" == "" ]] && export BUILD_USER="C2"
+
+(sudo useradd $BUILD_USER;) >/dev/null 2>&1
 set -e
-[[ ! -d ~C2/.BUILD ]] && sudo mkdir -p ~C2/.BUILD
+[[ -d ~$BUILD_USER/.BUILD ]] && sudo rm -rf ~$BUILD_USER/.BUILD
+[[ ! -d ~$BUILD_USER/.BUILD ]] && sudo mkdir -p ~$BUILD_USER/.BUILD
 
-sudo rsync $(cat BUILD_FILES.txt) ~C2/.BUILD/.
+sudo rsync $(cat BUILD_FILES.txt) ~$BUILD_USER/.BUILD/.
 
-sudo chown -R C2:C2 /home/C2/.BUILD
+sudo chown -R $BUILD_USER:$BUILD_USER /home/$BUILD_USER/.BUILD
+
+
+sudo -u $BUILD_USER -H -i sh -c "cd .BUILD && ./test_build.sh"
 
