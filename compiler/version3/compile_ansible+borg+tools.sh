c941addff3529fb3119cbaaa1d2152e64befcf63
root <admin@vpntech.net>
2020-04-13 10:46:06 -0400

    tweak script

--- compiler/version3/compile_ansible+borg+tools.sh
+++ compiler/version3/compile_ansible+borg+tools.sh
@@ -7,10 +7,11 @@ BUILT_DIR=~/BUILT101
         ## LATEST BUILT:
 #__BUILT_ID="$(cd $BUILT_DIR && ls -d *|sort -u|sed '1d' | sort -nr|  head -n1)"
         ## NEW BUILD:
-__BUILT_ID="$(date +%s)"
+export __BUILT_ID="$(date +%s)"
 
 
-STDOUT_FILE=$(mktemp)
+STDOUT_FILE=~/.stdout-tools
+
 [[ ! -d $BUILT_DIR ]] && mkdir -p $BUILT_DIR
 
 export ANSIBLE_MODE=1
@@ -20,19 +21,34 @@ export SCRIPTS_BUILD_MODE="ANSIBLE+BORGS+TOOLS"
 if [[ "$__DEBUG_MODE" == "1" ]]; then
     STDOUT_FILE=$(pwd)/.o
 else
-    source compile_common_run.sh | tee $STDOUT_FILE
+    _exec=compile_common_run.sh
+    source $_exec | tee $STDOUT_FILE
+    ec=$?
+    if [[ "$ec" != "0" ]]; then
+        ansi --red "Invalid Exit code $ec from \"$_exec\""
+        exit $ec
+    fi
 fi
 set -e
 
-COMBINED_TS="$(cat $STDOUT_FILE|  sed 's/\x1b\[[0-9;]*m//g'| tail -n1| cut -d'=' -f2| sed 's/-/\n/g'| tail -n1)"
+#COMBINED_TS="$(cat $STDOUT_FILE|  sed 's/\x1b\[[0-9;]*m//g'| tail -n1| cut -d'=' -f2| sed 's/-/\n/g'| tail -n1)"
+COMBINED_TS=$(cd ~/vpntech-ioncube-encoder/ansible-bundle-playbook-dev/compiler/version3 && borg list --format="{name}{NEWLINE}" --last 1| cut -d'-' -f2)
 
 
 
 
 BUILD_TOOLS_CMD="(cd ~/vpntech-ioncube-encoder && ./BUILD_TOOLS.sh -d $BUILT_DIR/$__BUILT_ID -m all)"
+echo $BUILD_TOOLS_CMD
+
+
+
 set +e
 eval $BUILD_TOOLS_CMD
 ec=$?
+if [[ "$ec" != "0" ]]; then
+    ansi --red "Invalid Exit code $ec from \"$BUILD_TOOLS_CMD\""
+    exit $ec
+fi
 set -e
 echo BUILD_TOOLS_CMD=$BUILD_TOOLS_CMD
 echo BUILD_TOOLS_CMD exited $ec
@@ -40,7 +56,7 @@ if [[ "$__BUILT_ID" == "" || "$ec" != "0" ]]; then
     ansi --red Invalid built
     exit 666
 fi
-_POST_CMD="(cd ~/vpntech-ioncube-encoder && time ./ADD_TOOLS.sh  -a .COMBINED-$COMBINED_TS --copy-tarball -t $BUILT_DIR/$__BUILT_ID)"
+_POST_CMD="(time ~/vpntech-ioncube-encoder/ADD_TOOLS.sh  -a .COMBINED-$COMBINED_TS --copy-tarball -t $BUILT_DIR/$__BUILT_ID)"
 echo _POST_CMD=$_POST_CMD
 
 set +e
--- compiler/version3/compile_tester.sh
+++ compiler/version3/compile_tester.sh
@@ -2,10 +2,51 @@
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source compile_common.sh
 
+
+__DEBUG_MODE=0
+BUILT_DIR=~/BUILT69691
+__BUILT_ID="$(date +%s)"
+
+STDOUT_FILE=$(mktemp)
+[[ ! -d $BUILT_DIR ]] && mkdir -p $BUILT_DIR
+
 export ANSIBLE_MODE=0
 export BUILD_BORG=0
 export SCRIPTS_BUILD_MODE=tester
 
+if [[ "$__DEBUG_MODE" == "1" ]]; then
+    STDOUT_FILE=$(pwd)/.o
+else
+    source compile_common_run.sh | tee $STDOUT_FILE
+fi
+set -e
 
-source compile_common_run.sh
+COMBINED_TS="$(cat $STDOUT_FILE|  sed 's/\x1b\[[0-9;]*m//g'| tail -n1| cut -d'=' -f2| sed 's/-/\n/g'| tail -n1)"
 
+
+
+BUILD_TOOLS_CMD="(cd ~/vpntech-ioncube-encoder && ./BUILD_TOOLS.sh -d $BUILT_DIR/$__BUILT_ID -m all)"
+set +e
+eval $BUILD_TOOLS_CMD
+ec=$?
+set -e
+echo BUILD_TOOLS_CMD=$BUILD_TOOLS_CMD
+echo BUILD_TOOLS_CMD exited $ec
+if [[ "$__BUILT_ID" == "" || "$ec" != "0" ]]; then
+    ansi --red Invalid built
+    exit 666
+fi
+_POST_CMD="(time ~/vpntech-ioncube-encoder/ADD_TOOLS.sh  -a .COMBINED-$COMBINED_TS -t $BUILT_DIR/$__BUILT_ID)"
+echo _POST_CMD=$_POST_CMD
+
+set +e
+eval $_POST_CMD
+ec=$?
+set -e
+echo _POST_CMD exited $ec
+if [[ "$ec" != "0" ]]; then
+    ansi --red Invalid built
+    exit $ec
+fi
+
+exit 0
